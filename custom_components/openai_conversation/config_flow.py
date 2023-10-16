69ab42f08a960795fe2681e67680d506cdda034a
Wolverine-14 <37383269+Wolverine-14@users.noreply.github.com>
2023-10-16 12:28:42 -0400

    Update config_flow.py

--- custom_components/openai_conversation/config_flow.py
+++ custom_components/openai_conversation/config_flow.py
@@ -13,7 +13,7 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.const import CONF_API_KEY
-from homeassistant.const import CONF_API_BASE
+from homeassistant.const import CONF_ADDRESS
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.helpers.selector import (
@@ -40,6 +40,7 @@ _LOGGER = logging.getLogger(__name__)
 
 STEP_USER_DATA_SCHEMA = vol.Schema(
     {
+        vol.Required(CONF_ADDRESS): str,
         vol.Required(CONF_API_KEY): str,
     }
 )
@@ -60,7 +61,7 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> None:
 
     Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
     """
-    openai.api_base = f'http://{data[CONF_API_BASE]}/v1'
+    openai.api_base = f'http://{data[CONF_ADDRESS]}/v1'
     openai.api_key = data[CONF_API_KEY]
     await hass.async_add_executor_job(partial(openai.Engine.list, request_timeout=10))
 
